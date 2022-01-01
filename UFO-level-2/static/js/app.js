1f5e593c43b239c030810f3ecb6922c8a632d0e2
Zachary Breen <zgbreen@gmail.com>
2022-01-01 23:57:17 +1100

    Update app.js

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,6 +1,5 @@
 // from data.js
 var tableData = data;
-
 // Use D3 to select the table body
 var tbody = d3.select("tbody");
 
@@ -44,10 +43,18 @@ function runEnter() {
   d3.event.preventDefault();
 
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+  var inputStartDate = d3.select("#start-datetime");
+  var inputCity = d3.select('#city')
+  var inputState = d3.select('#state')
+  var inputCountry = d3.select('#country')
+  var inputShape = d3.select('#shape')
 
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  var startDateValue = inputStartDate.property("value");
+  var cityValue = inputCity.property("value");
+  var stateValue = inputState.property("value");
+  var countryValue = inputCountry.property("value");
+  var shapeValue = inputShape.property("value");
 
   var tbody = d3.select("tbody");
   tbody.selectAll("tr").remove();
@@ -56,100 +63,40 @@ function runEnter() {
 
   // Use D3 to set the table class to `table table-striped`
   table.attr("class", "table table-striped");
-  
+
+  let filteredData = data;
   //run if statement for blank entry to show all results
-  if (inputValue != "") {
+  if (startDateValue != "" || stateValue != "" || cityValue != "" || countryValue != "" || shapeValue != "") {
 
-    console.log(inputValue);
-    var filteredData = data.filter(data => data.datetime === inputValue);
-    console.log(filteredData)
-    filteredData.forEach(({ datetime, city, state, country, shape, durationMinutes, comments }) => {
-
-      var row = tbody.append("tr");
-      // append the searched data 
-      row.append("td").text(datetime);
-      row.append("td").text(city);
-      row.append("td").text(state);
-      row.append("td").text(country);
-      row.append("td").text(shape);
-      row.append("td").text(durationMinutes);
-      row.append("td").text(comments);
-    });
-  } else {
-    
-    data.forEach(({ datetime, city, state, country, shape, durationMinutes, comments }) => {
-
-      //   // Append one table row per student/grade
-      var row = tbody.append("tr");
-
-      //   // append one cell for the student and one cell for the grade
-      row.append("td").text(datetime);
-      row.append("td").text(city);
-      row.append("td").text(state);
-      row.append("td").text(country);
-      row.append("td").text(shape);
-      row.append("td").text(durationMinutes);
-      row.append("td").text(comments);
-    });
-  };
-};
-
-var table_click = d3.select("#th");
-
-var table_sort = d3
-
-//sort table 
-function sortTable(n) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("myTable");
-  switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc";
-  /* Make a loop that will continue until
-  no switching has been done: */
-  while (switching) {
-      // Start by saying: no switching is done:
-      switching = false;
-      rows;
-      /* Loop through all table rows (except the
-      first, which contains table headers): */
-      for (i = 1; i < rows; i++) {
-          // Start by saying there should be no switching:
-          shouldSwitch = false;
-          /* Get the two elements you want to compare,
-          one from current row and one from the next: */
-          x = rows[i].getElementsByTagName("th")[n];
-          y = rows[i + 1].getElementsByTagName("th")[n];
-          /* Check if the two rows should switch place,
-          based on the direction, asc or desc: */
-          if (dir == "asc") {
-              if (tbody.toLowerCase() > tbody.toLowerCase()) {
-                  // If so, mark as a switch and break the loop:
-                  shouldSwitch = true;
-                  break;
-              }
-          } else if (dir == "desc") {
-              if (tbody.toLowerCase() < tbody.toLowerCase()) {
-                  // If so, mark as a switch and break the loop:
-                  shouldSwitch = true;
-                  break;
-              }
-          }
+    filteredData = data.filter(d => {
+      let include = true;
+      if (startDateValue) {
+        include = include && new Date(d.datetime).getTime() === new Date(startDateValue).getTime()
       }
-      if (shouldSwitch) {
-          /* If a switch has been marked, make the switch
-          and mark that a switch has been done: */
-          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-          switching = true;
-          // Each time a switch is done, increase this count by 1:
-          switchcount++;
-      } else {
-          /* If no switching has been done AND the direction is "asc",
-          set the direction to "desc" and run the while loop again. */
-          if (switchcount == 0 && dir == "asc") {
-              dir = "desc";
-              switching = true;
-          }
+      if (stateValue) {
+        include = include && d.state === stateValue
       }
+      if (cityValue) {
+        include = include && d.city === cityValue
+      }
+      if (shapeValue) {
+        include = include && d.shape === shapeValue
+      }
+      return include;
+    });
   }
+
+  console.log(filteredData)
+  filteredData.forEach(({ datetime, city, state, country, shape, durationMinutes, comments }) => {
+
+    var row = tbody.append("tr");
+    // append the searched data 
+    row.append("td").text(datetime);
+    row.append("td").text(city);
+    row.append("td").text(state);
+    row.append("td").text(country);
+    row.append("td").text(shape);
+    row.append("td").text(durationMinutes);
+    row.append("td").text(comments);
+  });
 };
